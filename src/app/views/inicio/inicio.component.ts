57d15d8a4babea04b0a8c6331b6361e8738a7004
Alejandro <alerod620@gmail.com>
2022-05-29 23:23:42 -0600

    Modificación al componente de geogebra

--- src/app/views/inicio/inicio.component.ts
+++ src/app/views/inicio/inicio.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+
+import { ApiService } from '../../services/api.service';
+import { Router } from '@angular/router';
+
 declare var GGBApplet: any;
 
 @Component({
@@ -8,10 +14,15 @@ declare var GGBApplet: any;
 })
 export class InicioComponent implements OnInit {
 
+  materialForm = new FormGroup({
+    material:new FormControl('',Validators.required),
+    enlace:new FormControl('',Validators.required)
+  })
+
   constructor() { }
 
   ngOnInit(): void {
-    var params = {"appName": "classic", "width": 900, "height": 450, "showToolBar": true, "showAlgebraInput": true, "showMenuBar": true };
+    var params = {"appName": "classic", "width": 900, "height": 500, "showToolBar": true, "showAlgebraInput": true, "showMenuBar": true };
     var applet = new GGBApplet(params, true);
     window.addEventListener("load", function() { 
         applet.inject('ggb-element');
